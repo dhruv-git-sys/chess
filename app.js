0028ba657941bd2b3177556ba6f44141a88c057d
Dhruv Agrawal <dhruvagrawal963@gmail.com>
2025-08-27 17:42:18 +0530

    moves

--- app.js
+++ app.js
@@ -22,16 +22,16 @@ io.on('connection',function(uniquesocket){
     log("Made socket connection");
     if(!players.white){
         players.white=uniquesocket.id;
-        uniquesocket.emit('playerRole','W');
+        uniquesocket.emit('playerRole','w');
     }
     else if(!players.black){
         players.black=uniquesocket.id;
-        uniquesocket.emit('playerRole','B');
+        uniquesocket.emit('playerRole','b');
     }
     else{
         uniquesocket.emit('playerRole','Spectator');
     }
-    socket.on("disconnect",(){
+    uniquesocket.on("disconnect",function(){
         if(uniquesocket.id===players.white){
             delete players.white;
         }
@@ -40,6 +40,32 @@ io.on('connection',function(uniquesocket){
         }
         else{
             log("A spectator disconnected");
+        }
+    });
+    uniquesocket.on('move',(move)=>{
+        try{
+            if(chess.turn()==='w'&&uniquesocket.id!==players.white){
+                return;
+            }
+            if(chess.turn()==='b'&&uniquesocket.id!==players.black){
+                return;
+            }
+            const result=chess.move(move);
+            if(result){
+                currentPlayer=chess.turn();
+                io.emit("move",move);
+                io.emit("boardState",chess.fen());
+            }
+            else{
+                log("Invalid move: ",move);
+                uniquesocket.emit("invalidMove",move);
+
+            }
+        }
+        catch(err){
+            log(err);
+            uniquesocket.emit("Invalid move: ",move);
+            
         }
     })
 });
